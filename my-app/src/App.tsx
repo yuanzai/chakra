cb038a806eaa5a3dae305b04ac9b958bdc2cf2bd
Junyuan Lau <junyuan@Junyuans-New-MBP.local>
2022-12-23 15:17:29 -0800

    Clean up app

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,27 +1,4 @@
 import * as React from "react"
-import {
-    ChakraProvider,
-    Box,
-    Button,
-    Text,
-    Link,
-    VStack,
-    Code,
-    Grid,
-    extendTheme,
-    SimpleGrid,
-    Card,
-    CardBody,
-    Center,
-    Flex,
-} from "@chakra-ui/react"
-import {SaasProvider} from '@saas-ui/react'
-import {AppShell} from '@saas-ui/app-shell'
-import {Sidebar, SidebarSection, NavItem} from '@saas-ui/sidebar'
-import {theme as baseTheme} from '@saas-ui/react'
-
-import {ColorModeSwitcher} from "./ColorModeSwitcher"
-import {Logo} from "./Logo"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import Home from "./components/pages/home";
